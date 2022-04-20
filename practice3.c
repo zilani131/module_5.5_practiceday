cf4b5962d9071cfaf704f56910fb54fd30cfc249
zilani131 <zilani.cuet@gmail.com>
2022-04-20 15:38:35 +0600

    practice 3 solving with if ,else

---
+++ practice3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter  a number ");
+    scanf("%d",&n);
+
+    if (n%2==0)
+    {
+        printf("%d is even number",n);
+    }
+    else 
+    {
+         printf("%d is odd number",n);
+    }
+    
+    return 0;
+}
